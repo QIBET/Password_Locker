74870780fb82340968ef95e9802e7fe360b89705
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-05-29 00:54:44 +0300

    Creating the save credentials test case

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ class Testclass (unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_user = User("Gabriel", "Odhiambo","0724835573","gabriel.odhiambo@ms.com", "Access")#first user object
+        self.new_credential = Credentials("facebook","admin","Access") #first credential object
     def test_create_user(self):
         '''
         Test to check whether a user is created successfully
@@ -32,11 +33,19 @@ class Testclass (unittest.TestCase):
         '''
         test to check whether a new instance of credentials can be created
         '''
-        self.new_credential = Credentials("facebook","admin","Access")
+       
 
         self.assertEqual(self.new_credential.account,"facebook")
         self.assertEqual(self.new_credential.username,"admin")
         self.assertEqual(self.new_credential.password,"Access")
+    def test_save_credentials(self):
+        '''
+        test to check whether user credentials are saved
+        '''
+        self.new_credential = Credentials("facebook","admin","Access")
+
+        self.new_credential.save_credentials()
+        self.assertEqual(len(Credentials.credential_List),1)
 
         
 if __name__ == '__main__':
