5ad221bb2c80edf7b1466292bad33ced2f8c1e2f
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-05-28 22:01:02 +0300

    Creating a user class blue print for users to use to create accounts

---
+++ user.py
@@ -0,0 +1,16 @@
+class User:
+    '''
+    User class to allows for creation of new instances of users
+    '''
+    def __init__(self,first_name,last_name,phone_number,email_address):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name: New user first name.
+            last_name : New user last name.
+            number: New user phone number.
+            email : New user email address.
+        '''
+        
