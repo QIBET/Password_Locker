f5f0ef204f567131188e52a8feb55d49352b4f91
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-05-28 22:25:59 +0300

    Creating the create test

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
 import unittest
 from user import User
 from credentials import Credentials 
+
+class Testclass (unittest.TestCase):
+    '''
+    Test class that defines test cases for the user/credentials class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Gabriel", "Odhiambo","0724835573","gabriel.odhiambo@ms.com", "Access")
