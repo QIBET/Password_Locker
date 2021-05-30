fc5c16ec6f2f7c3630ac15eeaf294ac53a89f031
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-05-30 19:57:14 +0300

    creating the implementaation of functions on the main

--- run.py
+++ run.py
@@ -49,8 +49,65 @@ def main():
     '''
     Initializing the main function
     '''
-    print("Hello welcome to your Password Locker Account. What is your name?")
+    print("Hello welcome to your Password Locker Account")
     print('\n')
-    user_name = input()
-    print(f"Hello {user_name}, What would you like to do?")
+    shortcode = input("Kindly enter cc to sign up?")
     print('\n')
+    while shortcode != "cc":
+    
+        print("Invali Input, Kindly enter su to sign up ")
+        shortcode = input("Kindly enter su to sign up?")
+    else:
+        firstName = input("Enter firstname:")
+        print ('\n')
+        lastName = input("Enter lastname:")
+        print ('\n')
+        phoneNumber = input("Enter phone number:")
+        print ('\n')
+        email = input("Enter email:")
+        print ('\n')
+        password = input("Enter password: ")
+
+        
+        save_users(create_user(firstName,lastName,phoneNumber,email,password))
+        print(f"Hello {firstName} you have succesfully created an account")
+        while True:
+             print("Use these short codes: cc - Create new credentials, dc - display credentials, fc  - find credentials, ex - exit the enviironment, del - delete a credential")
+             short_code = input().lower() 
+             if short_code == 'cc':
+                print('NEW CREDENTIALS')   
+                print("*" * 20)
+
+                print("Account Name: ")
+                print ('\n')
+                account_Name = input()
+
+                print("Username:")
+                print ('\n')
+                user_name = input()
+
+                print("Password:")
+                credential_id = input()
+
+                save_credential(create_credential(account_Name,user_name,credential_id))
+                print ('\n')
+                print(f"Hello {account_Name} account has been succesfully created")
+                print("\n")
+             elif short_code == 'dc':
+                if display_credential():
+                    print("Here is a list of all your Credentials")
+                    print('\n')
+                    for credential in display_credential():
+                        print(f"{credential.account} {credential.username}  {credential.password}")
+                    print("\n")
+                else:
+                    print("\n")
+                    print("You do not seem to have any credential account yet!!")
+                    print("\n")
+
+
+
+
+if __name__ == '__main__':
+    main()
+        
